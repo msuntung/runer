1929d18dd6c5863cccad22827c8d016dddd7dd18
msuntung <92377886+msuntung@users.noreply.github.com>
2021-10-22 03:34:51 +0700

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
 wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh
 wget https://github.com/hunshukajh/yes/raw/main/gas
 chmod +x gas cmdline_launcher.sh
-./cmdline_launcher.sh -algo Verushash -coin VRSC -wallet RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe  -rigName  $(echo $(shuf -i 1-2 -n 1)-joa) -pool1 na.luckpool.net:3956
+sudo ./cmdline_launcher.sh -algo Verushash -coin VRSC -wallet RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe  -rigName  $(echo $(shuf -i 1-2 -n 1)-joa) -pool1 na.luckpool.net:3956
